9b6cd8c0eb0769061ded2c8afeb58fea017705ad
Yosef <mercytv932@gmail.com>
2026-09-22 15:55:19 -0400

    error message and last polish

--- server.js
+++ server.js
@@ -12,9 +12,25 @@ async function mongoDbConnect() {
   try {
     await client.connect();
     console.log("Database connection successful!");
+    return true;
   } catch (error) {
     console.error("Mongodb connection Error:", error);
+    return false;
   }
 }
 
-mongoDbConnect();
+// Route
+app.get("/", async (req, res) => {
+  const connected = await mongoDbConnect(); //Is server connected to Mongodb?
+
+  if (connected) {
+    return res.json({ message: "Successfully connected to the database!" });
+  } else {
+    res.status(500).json({ message: "Failed to connect to the database." });
+  }
+});
+
+// Port
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); //start the server
